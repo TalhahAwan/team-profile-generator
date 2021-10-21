afc87371e63a9caa8560f4968c526c199d9f35de
Talhah Awan <talhahawan@gmail.com>
2021-10-20 23:54:28 -0500

    Created folders and added some js to tests

---
+++ test/intern.test.js
@@ -0,0 +1,7 @@
+const intern = require('./lib/intern')
+
+describe ("intern test", () =>{
+    describe("Initialization of intern test", () => {
+        it("Should create an intern object")
+    })
+});
